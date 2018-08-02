eba4027ccc8646975ef42077c2fbfa66b159346c
Ronak Mehta <ronakrm@gmail.com>
2018-08-02 15:51:30 -0500

    cleaned LQ, it works with -1 sample-. renamed simple, no t3f. how can we do SGD using manopt is next Q, plus NNs.

--- least_squares.py
+++ least_squares.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
-import t3f
+
+from OTTtfVariable import OTTtfVariable
 
 from pymanopt import Problem
 from pymanopt.solvers import SteepestDescent, TrustRegions
@@ -14,48 +15,22 @@ if __name__ == "__main__":
 	# y in R^25 (5*5)
 	# x in R^784 (28*28)
 	# W in 25x784
-	N = 10
-	dx = 4
-	dy = 4
-	d = 4
-	n = [0,2,2,2,2]
-	r = [1,2,2,2,1]
+	dx = 784
+	dy = 25
+	d = 6
+	n = [4,7,4,7,5,5]
+	r = [1,4,7,4,7,5,1]
 
 	np.random.seed(0)
 
-	X = np.random.randn(N,dx).astype('float32')
+	X = np.random.randn(1,dx).astype('float32')
 	W_gt = np.random.randn(dx,dy).astype('float32')
 	Y = X.dot(W_gt) #+ 0.01*np.random.randn(N,dy)
 
-
-	# Generate random init
-	#initializer = t3f.glorot_initializer([[5, 5, 5, 5], [4, 7, 4, 7]], tt_rank=7)
-	#W = t3f.get_variable('W', initializer=initializer) 
-
-	Q = []
-	for i in range(0,d):
-		Q.append( tf.Variable( tf.zeros([r[i]*n[i+1],r[i+1]]) ) )
-	Q.append( tf.Variable(tf.zeros(r[d])) ) # R
-
-	#Q1 = tf.Variable(tf.zeros([r0*dx,r1])) # 5 X 3
-	#Q2 = tf.Variable(tf.zeros([r1*dy,r2])) # 9 X 1
-	#R2 = tf.Variable(tf.zeros([r2])) # 1
-
-	U = []
-	for i in range(0,d):
-		U.append( tf.reshape(Q[i],[r[i],n[i+1],r[i+1]]) )
-	U[d-1] = tf.einsum('abc,c->abc',U[d-1],Q[d])
-
-	#U1 = tf.reshape(Q1,[r0,dx,r1]) # 1 X 5 X 3
-	#U2 = tf.reshape(Q2,[r1,dy,r2]) # 3 X 3 X 1
-	#U2 = tf.einsum('abc,c->abc',U2,R2) # 3 X 3 X 1
-
-	W_hTT = tf.einsum('abc,cde,efg,ghi->bdfh',*U)
-	W_hat = tf.reshape(W_hTT, [dx, dy])
+	W_hat = OTTtfVariable(opt_n=n, out_n=[dx,dy], r=r)
 
 	# Cost function is the sqaured test error
-	#W_hat = tf.einsum('abc,cde->bd',U1,U2) # dx x dy
-	Y_hat =	tf.matmul(X,W_hat)
+	Y_hat =	tf.matmul(X,W_hat.getW())
 	cost = tf.reduce_mean(tf.square(Y - Y_hat))
 
 	# first-order, second-order
@@ -65,28 +40,24 @@ if __name__ == "__main__":
 	# Product Manifold
 	PM = ()
 	for i in range(0,d):
-		PM = PM + (Stiefel(r[i]*n[i+1], r[i+1]),)
+		PM = PM + (Stiefel(r[i]*n[i], r[i+1]),)
 	PM = PM + (Euclidean(r[d]),)
 	manifold = Product( PM )
-	#manifold = Product(( Stiefel(r0*dx, r1), Stiefel(r1*dy, r2), Euclidean(r2) ))
-	#manifold = Product(( Euclidean(r0*n1, r1), Euclidean(r1*n2, r2), Euclidean(r2) ))
 
 	# Solve the problem with pymanopt
-	#args = Q.append(R)
-	problem = Problem(manifold=manifold, cost=cost, arg=Q, verbosity=2)
+	problem = Problem(manifold=manifold, cost=cost, arg=W_hat.getQ(), verbosity=2)
 	wopt = solver.solve(problem)
 
 	Uest = []
 	for i in range(0,d):
-		Uest.append( np.reshape(wopt[i],[r[i],n[i+1],r[i+1]]) )
+		Uest.append( np.reshape(wopt[i],[r[i],n[i],r[i+1]]) )
 	Uest.append(wopt[d])
 	print('W_est:')
-	W_est = np.reshape( np.einsum('abc,cde,efg,ghi,i->bdfh', *Uest), [dx,dy] )
+	W_est = np.reshape( np.einsum('abc,cde,efg,ghi,ijk,klm,m->bdfhjl', *Uest), [dx,dy] )
 	print( W_est )
 	print('W_gt:')
 	print(W_gt)
 
 	print('test')
-	tX = np.random.randn(3,dx).astype('float32')
-	print(tX.dot(W_est))
-	print(tX.dot(W_gt))
+	print(X.dot(W_est))
+	print(Y)
--- simple_ott.py
+++ simple_ott.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
 
+from OTTtfVariable import OTTtfVariable
+
 from pymanopt import Problem
 from pymanopt.solvers import SteepestDescent, TrustRegions
 from pymanopt.manifolds import Euclidean, Stiefel, Product
@@ -8,46 +10,43 @@ from pymanopt.manifolds import Euclidean, Stiefel, Product
 if __name__ == "__main__":
 
 	# This is just a 5x3 matrix with full rank
-	r = 3
-	r0 = 1
-	r1 = r
-	r2 = 1
-	tt_ranks = [r0,r1,r2]
+	r = [1,3,1]
 	n1 = 5
 	n2 = 3
-	Ns = [n1,n2]
-	d = len(Ns)
+	n = [n1,n2]
+	d = len(n)
 
+	np.random.seed(0)
 
 	# Generate random data
 	X_gt = np.random.randn(n1,n2).astype('float32')
 
-	Q1 = tf.Variable(tf.zeros([r0*n1,r1])) # 5 X 3
-	Q2 = tf.Variable(tf.zeros([r1*n2,r2])) # 9 X 1
-	R2 = tf.Variable(tf.zeros([r2])) # 1 X 1
+	X_hat = OTTtfVariable(opt_n=n, out_n=n, r=r)
 
-	U1 = tf.reshape(Q1,[r0,n1,r1]) # 1 X 5 X 3
-	U2 = tf.reshape(Q2,[r1,n2,r2]) # 3 X 3 X 1
-	U2 = tf.multiply(U2,R2) # 3 X 3 X 1
-
-	# Cost function is the sqaured test error
-	X_hat = tf.einsum('abc,cde->bd',U1,U2) # 5 X 3
-
-	cost = tf.reduce_mean(tf.square(X_hat - X_gt)) # scalar
+	cost = tf.reduce_mean(tf.square(X_hat.getW() - X_gt)) # scalar
 
 	# first-order, second-order
 	#solver = TrustRegions()
 	solver = SteepestDescent()
 
 	# Product Manifold
-	manifold = Product(( Stiefel(r0*n1, r1), Stiefel(r1*n2, r2), Euclidean(r2) ))
-	#manifold = Product(( Euclidean(r0*n1, r1), Euclidean(r1*n2, r2), Euclidean(r2) ))
+	PM = ()
+	for i in range(0,d):
+		PM = PM + (Stiefel(r[i]*n[i], r[i+1]),)
+	PM = PM + (Euclidean(r[d]),)
+	manifold = Product( PM )
 
 	# Solve the problem with pymanopt
-	problem = Problem(manifold=manifold, cost=cost, arg=[Q1,Q2,R2], verbosity=2)
+	problem = Problem(manifold=manifold, cost=cost, arg=X_hat.getQ(), verbosity=2)
 	wopt = solver.solve(problem)
+	
+	print(wopt)
 
+	Uest = []
+	for i in range(0,d):
+		Uest.append( np.reshape(wopt[i],[r[i],n[i],r[i+1]]) )
+	Uest.append(wopt[d])
 	print('X_est:')
-	print(np.einsum('abc,cde,e->bd', np.reshape(wopt[0],[r0,n1,r1]), np.reshape(wopt[1],[r1,n2,r2]), wopt[2]))
+	print( np.einsum('abc,cde,e->bd', *Uest) )
 	print('X_gt:')
 	print(X_gt)
