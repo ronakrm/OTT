7b9331ffab2a158f96e251b2cef95401c4c544ed
Ronak Mehta <ronakrm@gmail.com>
2018-11-11 23:55:59 -0600

    Added ADNI data processor, runner, visualizer. tf rnn needs to be edited to run adni, will not push that...seq viz comment update also.

--- adniVisualizer.py
+++ adniVisualizer.py
@@ -2,23 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-class seqVisualizer():
-    def __init__(self, batch_size, seqlen, frame_size):
+### DIMENSION OF 3D VOLUME
+dX = 121
+dY = 145
+dZ = 121
+
+class ADNIVisualizer():
+    def __init__(self, batch_size, seqlen, yslice):
         # Initialize subplots so we can use set_data for repeated plotting inside the loop
         nViz = np.min([int(batch_size), 12])
         self.nsrows = int(nViz/4)# 3
         self.nscols = int(nViz/3)# 4
 
+        self.yslice = yslice
+
         self.num_samples = self.nsrows * self.nscols
         self.nrows = self.nsrows * 2 # 2 here is gt and pred
         self.seqlen = seqlen
         self.ncols = self.nscols * self.seqlen
-        self.frame_size = frame_size
         self.fig, self.axarr = plt.subplots(self.nrows, self.ncols, figsize=(23,15)) 
         fig_arr = [[mpimg.AxesImage for j in range(self.ncols)] for i in range(self.nrows)]
         for ii in range(self.nrows):
             for jj in range(self.ncols):
-                rand_mat = np.random.rand(self.frame_size, self.frame_size)
+                rand_mat = np.random.rand(dX, dZ)
                 self.axarr[ii, jj].axis('off')
                 fig_arr[ii][jj] = self.axarr[ii, jj].imshow(rand_mat, cmap='gray')
 
@@ -35,10 +41,12 @@ class seqVisualizer():
 
                 # PRED
                 if r % 2 == 1:
-                    tmp = np.reshape(pd[sample,frame,:].T, [self.frame_size, self.frame_size])
+                    tmp = np.reshape(pd[sample,frame,:].T, [dX, dY, dZ])
+                    tmp = np.squeeze(tmp[:, self.yslice, :])
                 # GT
                 else:
-                    tmp = np.reshape(gt[sample,frame,:].T, [self.frame_size, self.frame_size])
+                    tmp = np.reshape(gt[sample,frame,:].T, [dX, dY, dZ])
+                    tmp = np.squeeze(tmp[:, self.yslice, :])
                 self.axarr[r, c].clear()
                 self.axarr[r, c].imshow(tmp, clim=(0.0, 1.0))
                 self.axarr[r, c].axis('off')
